db06cff5116158a1e225c4aa58c6cf4e5cc299a3
Oriyan Nadav <oriyanadav5@gmail.com>
2023-07-15 12:08:29 +1000

    update the new story form

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -6,7 +6,7 @@ from .models import NewsStory
 class StoryForm(ModelForm):
     class Meta:        
         model = NewsStory        
-        fields = ['title', 'author', 'pub_date', 'content', 'image']
+        fields = ['title', 'pub_date', 'content', 'image']
         widgets = {
             'pub_date': forms.DateInput(
                 format='%m/%d/%Y',
