db95d0e6da1a3a0ea3a3e0a23b185838b7c649ea
Oriyan Nadav <oriyanadav5@gmail.com>
2023-07-19 19:01:28 +1000

    removed seciton

--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -136,12 +136,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'news:index'
 LOGOUT_REDIRECT_URL = 'news:index'
-
-# Email configs
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
-# EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
-# ACCOUNT_EMAIL_VERIFICATION = 'none'
