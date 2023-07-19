41d142a81c9923a4885f2b0b50ed4d3230fc10ed
Oriyan Nadav <oriyanadav5@gmail.com>
2023-07-19 10:37:48 +1000

    reset password

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -16,7 +16,7 @@ class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
     email_template_name = 'users/password_reset_email.html'
     subject_template_name = 'users/password_reset_subject'
     success_message = "We've emailed you instructions for setting your password, " \
-                      "if an account exists with the email you entered. You should receive them shortly." \
-                      " If you don't receive an email, " \
-                      "please make sure you've entered the address you registered with, and check your spam folder."
+                        "if an account exists with the email you entered. You should receive them shortly." \
+                        " If you don't receive an email, " \
+                        "please make sure you've entered the address you registered with, and check your spam folder."
     success_url = reverse_lazy('users-home')
